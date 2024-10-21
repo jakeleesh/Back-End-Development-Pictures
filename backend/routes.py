9f03482c9a6de11229e4b1528dd812a3d8f826f0
Jake Lee <leeshaoherng@hotmail.com>
2024-10-21 10:45:44 -0400

    Exercise 5: Implement the PUT /picture endpoint

--- backend/routes.py
+++ backend/routes.py
@@ -74,7 +74,13 @@ def create_picture():
 
 @app.route("/picture/<int:id>", methods=["PUT"])
 def update_picture(id):
-    pass
+    # get data from the json body
+    picture_in = request.json
+    for index, picture in enumerate(data):
+        if picture["id"] == id:
+            data[index] = picture_in
+            return picture, 201
+    return {"message": "picture not found"}, 404
 
 ######################################################################
 # DELETE A PICTURE
